deec07b28b35442599d4306fffd4bc4b4cef3d10
Emrul-Hasan <imrulhasan003@gmail.com>
2022-09-17 00:32:12 +0600

    Update

--- Server/index.js
+++ Server/index.js
@@ -5,4 +5,7 @@ import cors from "cors";
 const app = express();
 const port = 5000;
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/",(req,res)  =>  res.send("Hello From Express"));
