80ab8c58ade090071ca91a70217875208c96122b
Emrul-Hasan <imrulhasan003@gmail.com>
2022-09-18 01:41:17 +0600

    RegisterPage

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,17 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import Register from './components/Register';
+import {Switch,Route, Routes} from "react-router-dom";
 
 function App() {
   return (
     <>
     <Navbar></Navbar>
-    <Home></Home>
+    <Routes>
+      <Route exact path='/' element={<Home />}/>
+      <Route exact path='/register' element={<Register />}/>
+    </Routes>
     </>
   );
 }
