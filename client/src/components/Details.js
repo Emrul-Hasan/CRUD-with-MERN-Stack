1d25dd7355d12a59ac4194c08676f0b13f12cb3f
Emrul-Hasan <imrulhasan003@gmail.com>
2022-09-27 14:30:45 +0600

    Update

--- client/src/components/Details.js
+++ client/src/components/Details.js
@@ -6,6 +6,8 @@ import CardContent from '@mui/material/CardContent';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import DraftsIcon from '@mui/icons-material/Drafts';
 import MapsHomeWorkIcon from '@mui/icons-material/MapsHomeWork';
+import PhoneIcon from '@mui/icons-material/Phone';
+import PlaceIcon from '@mui/icons-material/Place';
 
 
 const Details = () => {
@@ -13,24 +15,31 @@ const Details = () => {
     <div className="container mt-3">
         <h1 style={{ fontWeight:400 }}> Welcome to Emrul Hasan</h1>
         <Card sx={{ maxWidth: 600 }}>
-     
         <CardContent>
+          <div className="add-btn">
+          <button className='btn btn-primary mx-2'><BorderColorIcon></BorderColorIcon></button>
+           <button className='btn btn-danger'><DeleteForeverIcon></DeleteForeverIcon></button>
+          </div>
+          <div className="row">
+          <div className="left-view col-lg-6 col-md-6 col-12">
             <AccountCircleIcon></AccountCircleIcon>
-            <div className="left-view">
              <h3 className='mt-3'>Name: <span >Emrul Hasan</span></h3>
              <h3 className='mt-3'>Age: <span >Emrul Hasan</span></h3>
-             <p><DraftsIcon></DraftsIcon> Email: <span>jkdf@gmail.com</span></p>
-             <p><MapsHomeWorkIcon></MapsHomeWorkIcon>Occupation: <span>Someting</span></p>
+             <p className='mt-3'><DraftsIcon></DraftsIcon> Email: <span>jkdf@gmail.com</span></p>
+             <p className='mt-3'><MapsHomeWorkIcon></MapsHomeWorkIcon>Occupation: <span>Someting</span></p>
             </div>
-            <div className="right-view">
-                
+            <div className="right-view col-lg-6 col-md-6 col-12">
+               <p className='mt-5'><PhoneIcon></PhoneIcon>Phone : <span>7834578345</span></p> 
+               <p className='mt-3'><PlaceIcon></PlaceIcon>Location : <span>Dhaka</span></p>
+               <p className='mt-3'>Description: Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam cumque ratione quos nisi nostrum quis alias? Quia vitae vero repellendus?</p>
 
             </div>
+          </div>
+          
             
         </CardContent>
         </Card>
     </div>
   )
 }
-
 export default Details
